3c3d01a85de580bac7db686b8cb04b6e61307fae
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-03-12 23:54:51 -0300

    adding file

--- test-job.js
+++ test-job.js
@@ -1,27 +1,43 @@
-
+/*
+Dada una cadena que contiene solo los caracteres '(', "', 'K', 3, 'C' y 'I', determine si la cadena de entrada es válida.
+Una cadena de entrada es válida si:
+Los corchetes abiertos deben cerrarse con el mismo tipo de corchetes.
+Los corchetes abiertos deben cerrarse en el orden correcto.
+Cada paréntesis cerrado tiene un paréntesis abierto correspondiente del mismo tipo.
+*/
 s = ['(', ')', '{', '}', '[', ']']
 
 function isValid(s) {
-  let parentesis = {
-    "(": ")",
-    "{": "}",
-    "[": "]"
+  const stack = [];
+  const openBrackets = ['(', '{', '['];
+  const closeBrackets = [')', '}', ']'];
+  const brackets = {
+    '(': ')',
+    '{': '}',
+    '[': ']'
   }
-  Object.entries(parentesis).map((p) => {
-
-    const [key, value] = p;
-    console.log(p[0])
-    console.log(p[1])
-    if (!s.includes(key) && !s.includes(value)) {
-      console.log('es válida', p)
+  for (let i = 0; i < s.length; i++) {
+    if (openBrackets.includes(s[i])) {
+      stack.push(s[i]);
     } else {
-      console.log('no es válida', p)
+      const last = stack.pop();
+      if (s[i] !== brackets[last]) {
+        console.log('NO')
+        return 'NO';
+      }
     }
-
-  })
+  }
+  if (stack.length === 0) {
+    console.log('YES')
+    return 'YES';
+  } else {
+    console.log('NO')
+    return 'NO';
+  }
 }
 
-/* isValid('[]')
+isValid('[]')
 isValid("{}")
-isValid("()") */
+isValid("()")
 isValid("{")
+isValid("})")
